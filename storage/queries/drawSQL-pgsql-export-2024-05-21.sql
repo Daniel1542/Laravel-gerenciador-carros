1121e606602db4bfe283445b18fce51d33d97ed2
Daniel Henrique <queirozdaniel1999@gmail.com>
2024-05-23 15:51:48 -0300

    feat

--- storage/queries/drawSQL-pgsql-export-2024-05-21.sql
+++ storage/queries/drawSQL-pgsql-export-2024-05-21.sql
@@ -1,32 +1,37 @@
 CREATE TABLE "revisoes"(
     "id" BIGINT NOT NULL,
-    "id_revisao" BIGINT NOT NULL,
-    "data" VARCHAR(255) NOT NULL,
-    "tempo" INTEGER NOT NULL
+    "placa" VARCHAR(255) NOT NULL,
+    "data" DATE NOT NULL,
+    "descricao" VARCHAR(255) NOT NULL
 );
 ALTER TABLE
     "revisoes" ADD PRIMARY KEY("id");
+ALTER TABLE
+    "revisoes" ADD CONSTRAINT "revisoes_placa_unique" UNIQUE("placa");
 CREATE TABLE "proprietarios"(
     "id" BIGINT NOT NULL,
-    "id_proprietario" BIGINT NOT NULL,
     "nome" VARCHAR(255) NOT NULL,
-    "cpf" VARCHAR(255) NOT NULL,
+    "cpf" INTEGER NOT NULL,
     "sexo" VARCHAR(255) NOT NULL,
     "email" VARCHAR(255) NULL,
-    "telefone" INTEGER NOT NULL
+    "telefone" VARCHAR(255) NOT NULL
 );
 ALTER TABLE
     "proprietarios" ADD PRIMARY KEY("id");
+ALTER TABLE
+    "proprietarios" ADD CONSTRAINT "proprietarios_cpf_unique" UNIQUE("cpf");
 CREATE TABLE "veiculos"(
     "id" BIGINT NOT NULL,
-    "id_veiculo" BIGINT NOT NULL,
+    "cpf" INTEGER NOT NULL,
+    "placa" VARCHAR(255) NOT NULL,
     "modelo" VARCHAR(255) NOT NULL,
-    "marca" VARCHAR(255) NOT NULL,
-    "placa" VARCHAR(255) NOT NULL
+    "marca" VARCHAR(255) NOT NULL
 );
 ALTER TABLE
     "veiculos" ADD PRIMARY KEY("id");
 ALTER TABLE
-    "revisoes" ADD CONSTRAINT "revisoes_id_revisao_foreign" FOREIGN KEY("id_revisao") REFERENCES "veiculos"("id_veiculo");
+    "veiculos" ADD CONSTRAINT "veiculos_cpf_unique" UNIQUE("cpf");
 ALTER TABLE
-    "proprietarios" ADD CONSTRAINT "proprietarios_id_proprietario_foreign" FOREIGN KEY("id_proprietario") REFERENCES "veiculos"("id_veiculo");
+    "veiculos" ADD CONSTRAINT "veiculos_placa_foreign" FOREIGN KEY("placa") REFERENCES "revisoes"("placa");
+ALTER TABLE
+    "proprietarios" ADD CONSTRAINT "proprietarios_cpf_foreign" FOREIGN KEY("cpf") REFERENCES "veiculos"("cpf");
